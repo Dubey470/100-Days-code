310a828ab408b0ee3ed690fc71c80e90cb836c0c
prashant dubey <93030512+Dubey470@users.noreply.github.com>
2022-11-17 19:20:34 +0530

    Add files via upload

---
+++ Day13.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Day13
+{
+    public static void main(String args[])
+    {
+        int n,sum=0,product=1,l;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        n=sc.nextInt();
+        while(n>0)
+        {
+            l=n%10;
+            sum=sum+l;
+            product=product*l;
+            n=n/10;
+        }
+        if(sum==product)
+        System.out.println("the number is spy number");
+        else
+        {
+            System.out.println("the number is not a spy number");
+        }
+
+    }
+}
