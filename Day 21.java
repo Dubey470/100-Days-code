7071d0d87828e4ab30693b560e9e94ec6eee321c
prashant dubey <93030512+Dubey470@users.noreply.github.com>
2022-11-26 21:02:51 +0530

    Add files via upload

---
+++ Day 21.java
@@ -0,0 +1,29 @@
+/*
+ * Write the following code in your editor below:
+ *
+ * A class named Arithmetic with a method named add that takes 2 integers as parameters and returns an integer denoting their sum.
+ * A class named Adder that inherits from a superclass named Arithmetic.
+ * Your classes should not be be public.
+ */
+
+public class _02Inheritance2 {
+
+    public static void main(String[] args) {
+        Adder a = new Adder();
+
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+        
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
+}
+
+class Arithmetic {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
