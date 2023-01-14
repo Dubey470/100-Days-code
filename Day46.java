c30c399f554f712f844816e4c0a3c5c1d762f4a9
prashant dubey <93030512+Dubey470@users.noreply.github.com>
2023-01-14 21:58:36 +0530

    Add files via upload

---
+++ Day46.java
@@ -0,0 +1,21 @@
+//Java Program to Illustrate how User Authentication is Done
+import java.util.Scanner;
+public class Day46
+{
+    public static void main(String[] args)
+    {
+        String username,password;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter username");
+        username=sc.nextLine();
+        System.out.println("Enter Password");
+        password=sc.nextLine();
+        if(username.equals("user")&& password.equals("user"));
+        {
+            System.out.println("Authentication Successfull");
+        }
+        {
+            System.out.println("Authentication failed");
+        }
+    }
+}
